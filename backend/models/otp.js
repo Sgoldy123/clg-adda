4a80cce9214a8bbe2168c4e8d6caefe00cd24de2
Sahil Seli <sahilseli7236@gmail.com>
2021-04-02 16:39:33 +0530

    Otp changes

--- backend/models/otp.js
+++ backend/models/otp.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 
 
 const otpSchema=new mongoose.Schema({
-      createdAt: { type: Date, expires: '60s',  default: Date.now },
+    //   createdAt: { type: Date, expires: '3m',  default: Date.now },
         otp:{
             type:Number,
             required:true
         }       
-    })
-    // otpSchema.createIndex( { "lastModifiedDate": 1 }, { expireAfterSeconds: 30 } )
+    },{timestamps: true})
+    otpSchema.index( { "lastModifiedDate": 1 }, { expireAfterSeconds: 3600 } )
 
     const OTP = mongoose.model('OTP',otpSchema);
     export default OTP;
